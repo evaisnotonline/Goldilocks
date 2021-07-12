95ba341ae3021b84b3ddddd9d95e02c2b7d26df4
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-07-12 11:57:22 +0100

    Delete AppMorseCode.java

--- AppMorseCode.java
+++
@@ -1,9 +0,0 @@
-package MorseCode;
-
-public class AppMorseCode {
-
-	public static void main(String[] args) {
-		MorseTranslator translator = new MorseTranslator();
-		System.out.println(translator.decode(".--- .- ...- .- / .. ... / -.-. --- --- .-.. / --- -.-"));
-	}
-}
